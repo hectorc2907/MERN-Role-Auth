4c3affd1a9650d66a2e05411588afd349189a7ea
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-14 00:21:52 -0300

    configuramos un archivo index basico

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cookieparser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors());
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => res.send("test"));
 
-app.listen(4000,() => {
-    console.log("listen on port 4000")
-})
+app.listen(4000, () => {
+  console.log("listen on port 4000");
+});
