3b71bb28874725c7ce879c8af00d1e3eb85db625
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-14 14:11:27 -0300

    configuramos un registro basico

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log([name, email]);
+  };
+  return (
+    <>
+      <div className="register-container">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="passowrd"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Register</button>
+          <p className="register-link">
+            Already have an account? <Link to="/login">Login here</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
