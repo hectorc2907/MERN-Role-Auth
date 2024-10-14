437d881e639b0810c6448ba7b45ec616798962f0
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-14 13:48:17 -0300

    configuramos las rutas basicas de uso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Admin from "./pages/Admin.jsx";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +11,9 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </>
