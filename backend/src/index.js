1699d2e5af9f160a7416b695d49566fef271b0e1
29Hac07Cah96$ <hectorc2907@gmail.com>
2024-10-14 00:31:26 -0300

    realizamos una configuracion basica del index.js del backend y sus funciones

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,29 @@
+//importamos dependencias y herramientas del proyecto
 import express from "express";
-import cookieparser from "cookie-parser";
 import dotenv from "dotenv";
+import cookieparser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
 
+//habilitamos las variables de entorno
 dotenv.config();
 
+//configuramos la variable de entorno PORT como el puerto o el puerto 3000
+const PORT = process.env.PORT || 3000;
+
+//configuramos app como la ejecucion de la funcion express para facilitar la escritura
 const app = express();
 
+//configuramos la aplicacion de cada dependencia importada
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors());
 app.use(morgan("dev"));
 
+//ejecucion de rutas
 app.get("/", (req, res) => res.send("test"));
 
+//llamada del puerto
 app.listen(4000, () => {
   console.log("listen on port 4000");
 });
